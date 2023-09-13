e4b16e7ea07cd0b1fe5f386302bf7bff3e4224e3
farhanmaneri <farhanmaneri@gmail.com>
2023-09-13 10:56:37 -0700

    fetch data with useEffect

--- src/App.js
+++ src/App.js
@@ -1,42 +1,61 @@
-import { useState } from 'react';
-import './App.css';
-import{get} from './components/api/get'
+import { useEffect, useState } from "react";
+import "./App.css";
+import { get } from "./components/api/get";
 
 function App() {
-  let [todos, setTodos]= useState(null)
-let [error, setError] = useState(false)
-  
- const handleClick = async()=>{
-  try{
-  let data = await get()
-  // console.log(todos);
-  setTodos(data)
-}catch(error){
-  setError(true)
-}
+  let [todos, setTodos] = useState(null);
+  let [error, setError] = useState(false);
+  let [loading, setLoading] = useState(true);
 
-  if(todos === null)
-  return (<h1> loading ....</h1>)
-  else if(error === true){
-    return (<h1>something went wrong</h1>)
- }
- }
-  return (
-    <>
-    <div className="App">
-    <button onClick={handleClick}>Get Data</button>
-    <ul>
-      
-        {todos && todos.map(todo =>(
-      <div style={{color:todo.completed ? "red" : "green"}} key={todo.id}>{todo.id} {todo.title} </div>
-        )
-        )}
-      
-    </ul>
-    </div>
-   
+  useEffect( () => {
+    const fetchData = async () =>{
+    setLoading(true);
+    setError(false);
+    try {
+      let data = await get('todos');
+      console.log("loading todos" + todos);
+      setTodos(data);
+    } catch (error) {
+      setError(true);
+    } finally {
+      setLoading(false);
+    }}
+    fetchData()
+  }, []);
+  // const handleClick = async () => {
+  //   try {
+  //     let data = await get();
+  //     // console.log(todos);
+
+  //     setTodos(data);
+  //   } catch (error) {
+  //     setError(true);
+  //   } finally {
+  //     setLoading(false);
+  //   }
+  // };
+  if (loading === true) return <h1> loading ....</h1>;
+  else if (error === true) return <h1>something went wrong</h1>;
+  else if (todos.length === 0) return <h1>Todos is empty</h1>;
+  else
+    return (
+      <>
+        <div className="App">
+          {/* <button onClick={handleClick}>Get Data</button> */}
+          <ul>
+            {todos &&
+              todos.map((todo) => (
+                <div
+                  style={{ color: todo.completed ? "red" : "green" }}
+                  key={todo.id}
+                >
+                  {todo.id} {todo.title}{" "}
+                </div>
+              ))}
+          </ul>
+        </div>
       </>
-  );
+    );
 }
 
 export default App;
--- src/components/api/get.js
+++ src/components/api/get.js
@@ -1,6 +1,9 @@
 
-export const get = async () =>{
-let res = await fetch('https://jsonplaceholder.typicode.com/todos');
+export const get = async (apiEndPoint) =>{
+let res = await fetch('https://jsonplaceholder.typicode.com/'+apiEndPoint);
+if(!res.ok){
+    throw new Error(res.statusText);
+}
 let data = await res.json()
 console.log(data)
 return data
