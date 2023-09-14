353f32d33eb15574b38fbcead8fe064d8884874c
farhanmaneri <farhanmaneri@gmail.com>
2023-09-14 11:06:39 -0700

    data fetched with button

--- src/App.js
+++ src/App.js
@@ -3,59 +3,102 @@ import "./App.css";
 import { get } from "./components/api/get";
 
 function App() {
-  let [todos, setTodos] = useState(null);
+  // fetched data with button
+  let [users, setUsers] = useState(null);
   let [error, setError] = useState(false);
-  let [loading, setLoading] = useState(true);
+  let [loading, setLoading] = useState(false);
 
-  useEffect( () => {
-    const fetchData = async () =>{
+  const fetchUser = async () => {
     setLoading(true);
-    setError(false);
     try {
-      let data = await get('todos');
-      console.log("loading todos" + todos);
-      setTodos(data);
+      let res = await fetch("https://jsonplaceholder.typicode.com/users");
+      if (res.ok) {
+        let users =  await res.json();
+        setUsers(users);
+      }else{
+        throw new Error('Error in connection');
+      }
     } catch (error) {
-      setError(true);
+      setError(error.message);
     } finally {
       setLoading(false);
-    }}
-    fetchData()
-  }, []);
-  // const handleClick = async () => {
-  //   try {
-  //     let data = await get();
-  //     // console.log(todos);
+    }
+  };
 
+  
+    return (
+      <>
+        <di>
+          <h1> List of users</h1>
+        </di>
+        <button onClick={fetchUser}>fetch users</button>
+        <div>
+          {loading ? (
+            <h1>loading....</h1>
+          ) : error ? (
+            <h3> Error {error} </h3>
+          ) : (
+            users && users.map((user) => <h3 key={user.id}>{user.name}</h3>)
+          )}
+        </div>{" "}
+      </>
+    );
+
+  //fetched data with useEffect
+
+  // let [todos, setTodos] = useState(null);
+  // let [error, setError] = useState(false);
+  // let [loading, setLoading] = useState(true);
+
+  // useEffect( () => {
+  //   const fetchData = async () =>{
+  //   setLoading(true);
+  //   setError(false);
+  //   try {
+  //     let data = await get('todos');
+  //     console.log("loading todos" + todos);
   //     setTodos(data);
   //   } catch (error) {
   //     setError(true);
   //   } finally {
   //     setLoading(false);
-  //   }
-  // };
-  if (loading === true) return <h1> loading ....</h1>;
-  else if (error === true) return <h1>something went wrong</h1>;
-  else if (todos.length === 0) return <h1>Todos is empty</h1>;
-  else
-    return (
-      <>
-        <div className="App">
-          {/* <button onClick={handleClick}>Get Data</button> */}
-          <ul>
-            {todos &&
-              todos.map((todo) => (
-                <div
-                  style={{ color: todo.completed ? "red" : "green" }}
-                  key={todo.id}
-                >
-                  {todo.id} {todo.title}{" "}
-                </div>
-              ))}
-          </ul>
-        </div>
-      </>
-    );
+  //   }}
+  //   fetchData()
+  // }, []);
+  // // const handleClick = async () => {
+  // //   try {
+  // //     let data = await get();
+  // //     // console.log(todos);
+
+  // //     setTodos(data);
+  // //   } catch (error) {
+  // //     setError(true);
+  // //   } finally {
+  // //     setLoading(false);
+  // //   }
+  // // };
+  // if (loading === true) return <h1> loading ....</h1>;
+  // else if (error === true) return <h1>something went wrong</h1>;
+  // else if (todos.length === 0) return <h1>Todos is empty</h1>;
+  // else
+  //   return (
+  //     <>
+  //       <div className="App">
+  //         {/* <button onClick={handleClick}>Get Data</button> */}
+  //         <ul>
+  //           {todos &&
+  //             todos.map((todo) => (
+  //               <div
+  //                 style={{ color: todo.completed ? "red" : "green" }}
+  //                 key={todo.id}
+  //               >
+  //                 {todo.id} {todo.title}{" "}
+  //               </div>
+  //             ))}
+  //         </ul>
+  //       </div>
+  //     </>
+  //   );
 }
 
 export default App;
